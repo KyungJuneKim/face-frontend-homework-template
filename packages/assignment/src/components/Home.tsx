d9c7a815cfd850d88ccfded608ce701aeae56562
June <kjune1999@naver.com>
2022-06-28 01:18:52 +0900

    Create Home.tsx

---
+++ packages/assignment/src/components/Home.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import { css } from '@emotion/react';
+import Container from './Container';
+import createStyles from '../utils/createStyles';
+
+const useStyles = createStyles({
+  button: css({
+    height: '36px',
+    margin: '4px',
+  }),
+});
+
+export default function Home() {
+  const styles = useStyles();
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <button
+        type="button"
+        css={styles.button}
+        onClick={() => navigate('connection')}
+      >
+        회원가입
+      </button>
+      <button
+        type="button"
+        css={styles.button}
+        onClick={() => navigate('transaction')}
+      >
+        트랜잭션 전송
+      </button>
+    </Container>
+  );
+}
