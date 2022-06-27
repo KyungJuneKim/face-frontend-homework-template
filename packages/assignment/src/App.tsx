a0411d0d87b3401e2618ad806ec60d8515b749af
June <kjune1999@naver.com>
2022-06-28 01:19:17 +0900

    set router

--- packages/assignment/src/App.tsx
+++ packages/assignment/src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Connection from './components/connection/Connection';
+import RootLayout from './components/RootLayout';
+import TransactionSender from './components/transaction-sender/TransactionSender';
+import Home from './components/Home';
+
 function App() {
-  return <div />;
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/connection" element={<Connection />} />
+        <Route path="/transaction" element={<TransactionSender />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
