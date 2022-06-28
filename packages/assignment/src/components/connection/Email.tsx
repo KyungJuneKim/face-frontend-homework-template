93f6fb28b9458cd5c5c867eb7c031e55e83e346c
June <kjune1999@naver.com>
2022-06-28 21:50:53 +0900

    Update Email.tsx

--- packages/assignment/src/components/connection/Email.tsx
+++ packages/assignment/src/components/connection/Email.tsx
@@ -1,79 +1,18 @@
-import { css, keyframes } from '@emotion/react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { Fragment, useState } from 'react';
+import { css, useTheme } from '@emotion/react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { Fragment, useRef, useState } from 'react';
+import { FaceSDK } from '@face/sdk';
 import emailState from '../../store/emailState';
 import createStyles from '../../utils/createStyles';
 import signUpState from '../../store/signUpState';
-import faceSdkState from '../../store/faceSdkState';
-import circularProgress from '../../assets/images/pictures/circular_progress.png';
+import TextField from '../TextField';
+import CircularProgress from '../CircularProgress';
+import Header from '../Header';
+import Footer from '../Footer';
 
-const useStyles = createStyles({
-  title: css({
-    fontWeight: 700,
-    fontSize: '24px',
-    lineHeight: '36px',
-    color: '#313A45',
-    margin: '16px 0 0 0',
-  }),
-  subTitle: css({
-    fontSize: '14px',
-    lineHeight: '22px',
-    color: '#465365',
-    margin: 0,
-  }),
-  email: css({
-    height: '54px',
-    boxSizing: 'border-box',
-    padding: '14px 12px',
-    marginTop: '24px',
-    alignSelf: 'stretch',
-    borderWidth: '1px',
-    borderStyle: 'solid',
-    borderColor: '#D9E0E5',
-    borderRadius: '4px',
-    fontSize: '16px',
-    lineHeight: '24px',
-    letterSpacing: '0.001em',
-    color: '#3A4044',
-    resize: 'none',
-    '::placeholder': {
-      color: '#A1B2BE',
-    },
-    ':focus-visible': {
-      outline: 'none',
-      borderColor: '#3A4044',
-    },
-  }),
-  signButton: css({
-    padding: '14px 24px',
-    marginTop: '24px',
-    alignSelf: 'stretch',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '8px',
-    fontWeight: 700,
-    fontSize: '16px',
-    lineHeight: '24px',
-    letterSpacing: '0.001em',
-    border: 'none',
-    borderRadius: '4px',
-    backgroundColor: '#195DEE',
-    color: '#FFFFFF',
-    ':disabled': {
-      backgroundColor: '#D7DBE0',
-      color: '#737E91',
-    },
-  }),
-  loading: css({
-    animationName: keyframes({
-      from: {transform: 'rotate(0deg)' },
-      to: {transform: 'rotate(360deg)' },
-    }),
-    animationDuration: '1s',
-    animationIterationCount: 'infinite',
-    animationTimingFunction: 'linear',
-  }),
+const useStyles = createStyles((theme) => ({
+  title: css([theme.styles.title, { margin: '16px 0 0 0' }]),
+  signButton: css([theme.styles.button, { marginTop: '24px', gap: '8px' }]),
   description: css({
     margin: '16px 0 0 0',
     fontSize: '12px',
@@ -81,57 +20,60 @@ const useStyles = createStyles({
     letterSpacing: '0.01em',
     color: '#919DB6',
   }),
-  link: css({
-    textDecoration: 'none',
-  }),
-});
+}));
 
 export default function Email() {
   const styles = useStyles();
+  const theme = useTheme();
   const [loading, setLoading] = useState(false);
-  const sdk = useRecoilValue(faceSdkState);
+  const sdk = useRef(
+    new FaceSDK('https://ropsten.infura.io/v3/2a4f59ea8b174fb7ae9ed6fae1137e59')
+  );
   const [id, setId] = useRecoilState(emailState);
   const setSignUp = useSetRecoilState(signUpState);
 
   return (
     <Fragment>
+      <Header />
       <h1 css={styles.title}>Connect Face Wallet</h1>
-      <h2 css={styles.subTitle}>Enter the world of Web3 with Face</h2>
-      <textarea
-        css={styles.email}
-        value={id}
-        onChange={(event) => setId(event.target.value)}
-        placeholder="satoshi@facewallet.xyz"
-      />
+      <h2 css={theme.styles.subTitle}>Enter the world of Web3 with Face</h2>
+      <div css={css({ marginTop: '24px' })}>
+        <TextField
+          value={id}
+          onChange={(event) => setId(event.target.value)}
+          placeholder="satoshi@facewallet.xyz"
+        />
+      </div>
       <button
         type="button"
         css={styles.signButton}
         disabled={!id || loading}
         onClick={async () => {
           setLoading(true);
-          if (await sdk.checkEmail(id)) setSignUp('Verification');
+          if (await sdk.current.checkEmail(id)) setSignUp('Verification');
           setLoading(false);
         }}
       >
-        {loading && <img css={styles.loading} src={circularProgress} alt="" />}
+        {loading && <CircularProgress />}
         Sign up / Login
       </button>
       <p css={styles.description}>
         By clicking the button, I acknowledge that I have read and agree to Face
         Wallet&apos;s&nbsp;
-        <a css={styles.link} href="https://haechi.io/ko">
+        <a css={theme.styles.link} href="https://haechi.io/ko">
           Term of Service
         </a>
         ,&nbsp;
-        <a css={styles.link} href="https://haechi.io/ko">
+        <a css={theme.styles.link} href="https://haechi.io/ko">
           Privacy Policy
         </a>
         &nbsp; and&nbsp;
-        <a css={styles.link} href="https://haechi.io/ko">
+        <a css={theme.styles.link} href="https://haechi.io/ko">
           Cookie Policy
         </a>
         &nbsp; in their entirety.
       </p>
+      <Footer />
     </Fragment>
   );
 }
