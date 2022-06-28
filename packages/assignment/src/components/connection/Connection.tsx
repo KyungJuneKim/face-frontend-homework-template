48b2f666953291c761a5854a5642128c8b0a3291
June <kjune1999@naver.com>
2022-06-28 21:52:08 +0900

    Update Connection.tsx

--- packages/assignment/src/components/connection/Connection.tsx
+++ packages/assignment/src/components/connection/Connection.tsx
@@ -5,8 +5,6 @@ import signUpState from '../../store/signUpState';
 import emailState from '../../store/emailState';
 import { SignUpState } from '../../types/signUpState';
 import Email from './Email';
-import Header from '../Header';
-import Footer from '../Footer';
 
 const signUpComponents: Record<SignUpState, ReactNode> = {
   Email: <Email />,
