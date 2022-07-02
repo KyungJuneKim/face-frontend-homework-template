c2925ee6f6af9f7e31202eb0deb338c77d77feb3
June <kjune1999@naver.com>
2022-07-02 14:03:54 +0900

    get receiver address

--- packages/assignment/src/components/Home.tsx
+++ packages/assignment/src/components/Home.tsx
@@ -1,27 +1,52 @@
 import { useNavigate } from 'react-router-dom';
-import { css } from '@emotion/react';
+import { css, useTheme } from '@emotion/react';
+import { useRecoilState } from 'recoil';
 import Container from './Container';
 import createStyles from '../utils/createStyles';
+import receiverAddressState from '../store/receiverAddressState';
+import TextField from './TextField';
 
-const useStyles = createStyles((theme) => ({
-  button: css([theme.styles.button, { margin: '4px' }]),
-}));
+const useStyles = createStyles({
+  margin: css({ margin: '2px 0' }),
+  divider: css({
+    height: '1px',
+    backgroundColor: '#313A45',
+  }),
+});
 
 export default function Home() {
   const styles = useStyles();
+  const theme = useTheme();
   const navigate = useNavigate();
+  const [receiver, setReceiver] = useRecoilState(receiverAddressState);
+
   return (
     <Container>
       <button
         type="button"
-        css={styles.button}
+        css={[theme.styles.button, styles.margin]}
         onClick={() => navigate('connection')}
       >
         회원가입
       </button>
+      <span css={[styles.divider, css({ margin: '12px 0' })]} />
+      <div css={styles.margin}>
+        <TextField
+          value={receiver}
+          onChange={(event) => setReceiver(event.target.value)}
+          placeholder="receiver address"
+        />
+      </div>
+      <div css={styles.margin}>
+        <TextField
+          value={receiver}
+          onChange={(event) => setReceiver(event.target.value)}
+          placeholder="eth amount for transmission"
+        />
+      </div>
       <button
         type="button"
-        css={styles.button}
+        css={[theme.styles.button, styles.margin]}
         onClick={() => navigate('transaction')}
       >
         트랜잭션 전송
