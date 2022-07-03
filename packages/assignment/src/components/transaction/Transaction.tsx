f7f1ecb60e98590e822ca006043935c39ee3d44f
June <kjune1999@naver.com>
2022-07-03 14:03:22 +0900

    Update Transaction.tsx

--- packages/assignment/src/components/transaction/Transaction.tsx
+++ packages/assignment/src/components/transaction/Transaction.tsx
@@ -1,12 +1,22 @@
+import { ReactNode, useEffect } from 'react';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
 import Container from '../Container';
-import Header from '../Header';
-import Footer from '../Footer';
+import Sender from './Sender';
+import { TransactionStep } from '../../types/TransactionStep';
+import transactionStepState from '../../store/transactionStepState';
+
+const transactionComponents: Record<TransactionStep, ReactNode> = {
+  Sender: <Sender />,
+  Bill: undefined,
+};
 
 export default function Transaction() {
-  return (
-    <Container>
-      <Header />
-      <Footer />
-    </Container>
-  );
+  const transactionStep = useRecoilValue(transactionStepState);
+  const resetTransactionStep = useResetRecoilState(transactionStepState);
+
+  useEffect(() => {
+    resetTransactionStep();
+  }, [resetTransactionStep]);
+
+  return <Container>{transactionComponents[transactionStep]}</Container>;
 }
